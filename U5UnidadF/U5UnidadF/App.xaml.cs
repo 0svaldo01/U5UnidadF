317b975afedc555877960063043edb76f9d74da8
Victor Rodriguez <Victor Rodriguez@DESKTOP-VR8AAQA>
2022-06-09 20:42:48 -0500

    Agregar archivos de proyecto.

---
+++ U5UnidadF/U5UnidadF/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using U5UnidadF.Views; 
+
+namespace U5UnidadF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ListaPeliculasView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
